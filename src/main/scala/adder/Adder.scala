a76ef43c28dec3026889fcc0ddaa77c16fbd2ffe
Justin Deters <justindeters@gmail.com>
2020-07-06 15:03:54 -0500

    created a parameterized ripple carry adder with testing

--- src/main/scala/adder/Adder.scala
+++ src/main/scala/adder/Adder.scala
@@ -9,8 +9,36 @@ class Adder (bitWidth: Int) extends Module {
   val io = IO(new Bundle{
     val a = Input(UInt(bitWidth.W))
     val b = Input(UInt(bitWidth.W))
-    val sum = Output(UInt(bitWidth.W+1))
+    val sum = Output(UInt(bitWidth.W + 1))
   })
 
-  io.sum := io.a +& io.b
+  //create each adder and wire them to their inputs
+  val adders = for (i <- 0 until bitWidth) yield {
+    val unit = Module(new OneBitAdder())
+    //wire up the inputs
+    unit.io.a := io.a(i)
+    unit.io.b := io.b(i)
+
+    unit
+  }
+
+  //the first adder needs to have a false input to carryIn
+  adders(0).io.carryIn := false.B
+
+  //wire the carryOut from n-1 to the carryIn of n
+  for(i <- 1 until bitWidth) {
+    adders(i).io.carryIn := adders(i-1).io.carryOut
+  }
+
+  //create a set of wires to collect the sums
+  val sums = Wire(Vec(bitWidth + 1, Bool()))
+
+  //collect the sums
+  for(i <- 0 until bitWidth) sums(i) := adders(i).io.sum
+
+  //get the carry out of the last adder to complete the sum
+  sums.last := adders.last.io.carryOut
+
+  //wire up the full sum to the output
+  io.sum := sums.asUInt
 }
---
+++ src/main/scala/adder/OneBitAdder.scala
@@ -0,0 +1,20 @@
+package adder
+
+import chisel3._
+
+class OneBitAdder extends Module {
+  val io = IO(new Bundle{
+    val a = Input(Bool())
+    val b = Input(Bool())
+    val carryIn = Input(Bool())
+    val sum = Output(Bool())
+    val carryOut = Output(Bool())
+  })
+
+  val p = io.a ^ io.b
+
+  io.sum := p ^ io.carryIn
+  val g = io.a & io.b
+  val p_c = io.carryIn & p
+  io.carryOut := g | p_c
+}
