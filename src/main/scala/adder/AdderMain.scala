2e7b1b7c307d70680ea0a4f2ae84a922982975a9
Justin Deters <justindeters@gmail.com>
2021-01-07 13:31:05 -0600

    added loop to quickly generate full design space

--- src/main/scala/adder/AdderMain.scala
+++ src/main/scala/adder/AdderMain.scala
@@ -5,13 +5,18 @@ import chisel3.stage.{ChiselStage, ChiselGeneratorAnnotation}
 import chisel3.aop.injecting.InjectingAspect
 
 object AdderMain extends App {
-  private val numBits = 120
-  private val numSubAdders = 2
+  val numBits = 120
 
-  val design = ChiselGeneratorAnnotation(() => new TopAdder(numBits, numSubAdders))
-  val aspects = (new AdderAspects(numBits,numSubAdders)).hybridCarry
+  val subAdderList = List(1,2,3,4,5,6,8,10,12,15,20,24,30);
 
-  (new chisel3.stage.ChiselStage).execute(
-    Array("-X", "verilog"),
-    aspects ++ Seq(design))
+  for(a <- subAdderList){
+
+    val design = ChiselGeneratorAnnotation(() => new TopAdder(numBits, a))
+    val aspects = (new AdderAspects(numBits, a)).hybridCarry
+
+    (new chisel3.stage.ChiselStage).execute(
+      Array("-X", "verilog", "-o", "Adder_" + numBits + "_" + a),
+      aspects ++ Seq(design))
+
+  }
 }
